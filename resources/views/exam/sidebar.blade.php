07944e4a91cf1e56f3a8f09b09e482e086d7e8fd
ajamiscosa <ajamiscosa@gmail.com>
2015-06-21 00:38:56 +0800

    cleanup

---
+++ resources/views/exam/sidebar.blade.php
@@ -0,0 +1,35 @@
+<div class="col-md-3">
+    <div class="box box-solid">
+        <div class="box-header with-border">
+            <h3 class="box-title">Folders</h3>
+            <div class='box-tools'>
+                <button class='btn btn-box-tool' data-widget='collapse'><i class='fa fa-minus'></i></button>
+            </div>
+        </div>
+        <div class="box-body no-padding">
+            <ul class="nav nav-pills nav-stacked">
+                <li><a href="/mail/inbox"><i class="fa fa-inbox"></i> Inbox <span class="label label-primary pull-right">12</span></a></li>
+                <li><a href="/mail/sent"><i class="fa fa-envelope-o"></i> Sent</a></li>
+                <li><a href="/mail/drafts"><i class="fa fa-file-text-o"></i> Drafts</a></li>
+                <li><a href="/mail/junk"><i class="fa fa-filter"></i> Junk <span class="label label-waring pull-right">65</span></a></li>
+                <li><a href="/mail/trash"><i class="fa fa-trash-o"></i> Trash</a></li>
+            </ul>
+        </div><!-- /.box-body -->
+    </div><!-- /. box -->
+    <div class="box box-solid">
+        <div class="box-header with-border">
+            <h3 class="box-title">Labels</h3>
+            <div class='box-tools'>
+                <button class='btn btn-box-tool' data-widget='new' data-toggle='tooltip' title='New'><i class='fa fa-pencil'></i></button>
+                <button class='btn btn-box-tool' data-widget='collapse'><i class='fa fa-minus'></i></button>
+            </div>
+        </div>
+        <div class="box-body no-padding">
+            <ul class="nav nav-pills nav-stacked">
+                <li><a href="/mail/label/important"><i class="fa fa-circle-o text-green"></i> Important</a></li>
+                <li><a href="/mail/label/submissions"><i class="fa fa-circle-o text-navy"></i> Pending Submissions</a></li>
+                <li><a href="/mail/label/requests"><i class="fa fa-circle-o text-maroon"></i> Clone Requests</a></li>
+            </ul>
+        </div><!-- /.box-body -->
+    </div><!-- /.box -->
+</div><!-- /.col -->
